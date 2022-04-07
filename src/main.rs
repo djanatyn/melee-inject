5bc048a8ce8b2f7c28f6d083f297c1cb4b5cd6e8
Jonathan Strickland <djanatyn@gmail.com>
2022-04-07 08:48:52 -0400

    snapshot files in melee iso

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
 use gc_gcm::GcmFile;
 use std::io;
 
-fn main() -> io::Result<()> {
-    let iso = GcmFile::open("ssbm.iso").expect("could not open ISO");
+const ISO_PATH: &str = "ssbm.iso";
 
-    let files = iso
-        .filesystem
+fn all_files(iso: &'_ GcmFile) -> impl Iterator<Item = &'_ gc_gcm::FsNode> {
+    iso.filesystem
         .files
         .iter()
-        .filter(|e| matches!(e, gc_gcm::FsNode::File { .. }));
+        .filter(|e| matches!(e, gc_gcm::FsNode::File { .. }))
+}
+
+fn main() -> io::Result<()> {
+    let iso = GcmFile::open(ISO_PATH).expect("could not open ISO");
+    let files = all_files(&iso).collect::<Vec<_>>();
 
     println!("{files:#?}");
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{all_files, ISO_PATH};
+    use gc_gcm::GcmFile;
+    use insta;
+
+    #[test]
+    fn load_iso() {
+        GcmFile::open(ISO_PATH).expect("could not open ISO");
+    }
+
+    #[test]
+    fn load_files() {
+        let iso = GcmFile::open(ISO_PATH).expect("could not open ISO");
+        let files = all_files(&iso).collect::<Vec<_>>();
+        insta::assert_debug_snapshot!(files);
+    }
 }
