f05fff8c186c372f6ee877a76dbf9b1680909870
Jonathan Strickland <djanatyn@gmail.com>
2022-06-22 08:05:39 -0400

    codegen: parse all game filenames (including kirby)

--- melee_inject_codegen/src/main.rs
+++ melee_inject_codegen/src/main.rs
@@ -2,6 +2,7 @@
 
 use codegen::Scope;
 use gc_gcm::{FsNode, GcmFile};
+use std::collections::HashMap;
 
 const SSBM_ISO: &str = "ssbm.iso";
 
@@ -12,29 +13,29 @@ use std::str::FromStr;
 /// This information was foudn from DRGN's DTW:
 /// <https://github.com/DRGN-DRC/DAT-Texture-Wizard/blob/75d5c5c2cfbedb8ecda2e8a3db8cdb083fb6b892/DAT%20Texture%20Wizard.py#L175>
 const CHARACTER_PREFIXES: phf::Map<&'static str, &'static str> = phf_map! {
-    "Bo" => "[Boy] Male Wireframe",
+    "Bo" => "Male Wireframe",
     "Ca" => "Captain Falcon",
     "Ch" => "Crazy Hand",
-    "Cl" => "Child/Young Link",
-    "Co" => "Common to the cast",
+    "Cl" => "Young Link",
+    "Co" => "Common",
     "Dk" => "Donkey Kong",
     "Dr" => "Dr. Mario",
     "Fc" => "Falco",
-    "Fe" => "[Fire Emblem] Roy",
+    "Fe" => "Roy",
     "Fx" => "Fox",
-    "Gk" => "[GigaKoopa] GigaBowser",
-    "Gl" => "[Girl] Female Wireframe",
+    "Gk" => "GigaBowser",
+    "Gl" => "Female Wireframe",
     "Gn" => "Ganondorf",
     "Gw" => "Game 'n Watch",
     "Ic" => "Ice Climbers",
     "Kb" => "Kirby",
-    "Kp" => "[Koopa] Bowser",
+    "Kp" => "Bowser",
     "Lg" => "Luigi",
     "Lk" => "Link",
     "Mh" => "Master Hand",
     "Mn" => "Menus Data",
     "Mr" => "Mario",
-    "Ms" => "[Mars] Marth",
+    "Ms" => "Marth",
     "Mt" => "Mewtwo",
     "Nn" => "[Nana] Ice Climbers",
     "Ns" => "Ness",
@@ -52,16 +53,34 @@ const CHARACTER_PREFIXES: phf::Map<&'static str, &'static str> = phf_map! {
     "Zd" => "Zelda",
 };
 
+const COLORS: phf::Map<&'static str, &'static str> = phf_map! {
+    "Aq" => "aqua",
+    "Bk" => "black",
+    "Bu" => "blue",
+    "Gr" => "green",
+    "Gy" => "gray",
+    "La" => "lavender",
+    "Nr" => "neutral",
+    "Or" => "orange",
+    "Pi" => "pink",
+    "Re" => "red",
+    "Wh" => "white",
+    "Ye" => "yellow",
+};
+
 #[derive(Debug)]
 struct CharacterFile {
     filename: String,
     name: String,
+    color: Option<String>,
 }
 
 impl FromStr for CharacterFile {
     type Err = io::Error;
 
     fn from_str(filename: &str) -> io::Result<Self> {
+        println!("{filename}");
+
         // strip Pl prefix
         let prefix_stripped = filename
             .strip_prefix("Pl")
@@ -70,13 +89,93 @@ impl FromStr for CharacterFile {
         // take next two characters
         let char_code: String = prefix_stripped.chars().take(2).collect();
 
+        // match against character prefixes
         let name = CHARACTER_PREFIXES
             .get(&char_code)
-            .expect("failed to find prefix: {char_code}");
+            .expect("failed to find prefix");
+
+        // take remaining characters
+        let remaining = prefix_stripped
+            .strip_prefix(&char_code)
+            .expect("failed to strip after character prefix")
+            .strip_suffix(".dat")
+            .expect("failed to strip .dat suffix");
+
+        // return on animations and common files
+        if remaining.len() == 0 || remaining == "AJ" || remaining == "DViWaitAJ" {
+            return Ok(CharacterFile {
+                filename: filename.to_string(),
+                name: name.to_string(),
+                color: None,
+            });
+        }
+
+        // special handling for Kirby
+        if char_code == "Kb" {
+            // 3 types of Kirby file:
+            // - PlKb.dat (covered previously)
+            // - PlKb<COLOR>.dat
+            // - PlKb<COLOR>Cp<CHAR_CODE>
+            // - PlKbCp<CHAR_CODE>.dat
+
+            return match remaining.len() {
+                // <COLOR>
+                2 => {
+                    let color = COLORS.get(&remaining).expect("failed to match color");
+
+                    Ok(CharacterFile {
+                        filename: filename.to_string(),
+                        name: name.to_string(),
+                        color: Some(color.to_string()),
+                    })
+                }
+                // Cp<CHAR_CODE>
+                4 => {
+                    let copied_char_code = remaining
+                        .strip_prefix("Cp")
+                        .expect("failed on char-specific kirby copy power file");
+
+                    let copied_char = CHARACTER_PREFIXES
+                        .get(&copied_char_code)
+                        .expect("failed to find kirby copied char");
+
+                    Ok(CharacterFile {
+                        filename: filename.to_string(),
+                        name: format!("Kirby Copy Power [{copied_char}]"),
+                        color: None,
+                    })
+                }
+                // <COLOR>Cp<CHAR_CODE>
+                6 => {
+                    let color_code: String = remaining.chars().take(2).collect();
+                    let color = COLORS.get(&color_code).expect("failed to match color");
+
+                    let copied_char_code = remaining
+                        .strip_prefix(&color_code)
+                        .expect("failed to strip color")
+                        .strip_prefix("Cp")
+                        .expect("failed to find kirby character code");
+
+                    let copied_char = CHARACTER_PREFIXES
+                        .get(&copied_char_code)
+                        .expect("failed to find kirby copied char");
+
+                    Ok(CharacterFile {
+                        filename: filename.to_string(),
+                        name: format!("Kirby Copy Power [{copied_char}]"),
+                        color: Some(color.to_string()),
+                    })
+                }
+                _ => panic!("unexpected kirby file"),
+            };
+        }
+
+        let color = COLORS.get(&remaining).expect("failed to match color");
 
         Ok(CharacterFile {
             filename: filename.to_string(),
             name: name.to_string(),
+            color: Some(color.to_string()),
         })
     }
 }
@@ -88,7 +187,7 @@ fn main() {
     let iso = GcmFile::open(SSBM_ISO).expect("could not open ISO");
 
     let mut scope = Scope::new();
-    let mut characters: Vec<CharacterFile> = Vec::new();
+    let mut characters: HashMap<String, Vec<CharacterFile>> = HashMap::new();
 
     for node in iso.filesystem.files {
         // find all file entries (skip directories)
@@ -97,27 +196,28 @@ fn main() {
             _ => continue,
         };
 
-        // skip files that don't end in .dat
+        // skip files that dont end in .dat
         if !name.ends_with(".dat") {
             continue;
         }
 
         // character names start with "Pl-"
         if name.starts_with("Pl") {
-            let file = name
-                .parse::<CharacterFile>()
-                .expect("failed to parse {name}");
-            characters.push(file);
+            let file = name.parse::<CharacterFile>().expect("failed to parse");
+
+            match characters.get_mut(&file.name) {
+                Some(files) => files.push(file),
+                None => {
+                    let key = file.name.clone();
+                    characters.insert(key, vec![file]);
+                }
+            };
         }
 
         // scope.new_struct(&name);
     }
 
-    let groupings = characters
-        .group_by(|a, b| a.name == b.name)
-        .collect::<Vec<_>>();
-
-    println!("{groupings:#?}");
+    println!("{characters:#?}");
 
     // let output = scope.to_string();
 }
